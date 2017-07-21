94084b6969f57c573c35ccba555139e34838c814
Tyler Haigh <tylerhaigh1994@me.com>
2017-07-22 09:44:19 +1000

    Update port for Heroku

--- tssrc/server.ts
+++ tssrc/server.ts
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000; // Heroku port override
+
 var app = express()
 
 hbs.registerPartials(__dirname + '/../views/partials'); // need to use /../ to go up one folder
@@ -60,4 +62,4 @@ app.get('/bad', (req, res) => {
     })
 })
 
-app.listen(3000, () => { console.log('Server is running on port 3000') });
+app.listen(port, () => { console.log(`Server is running on port ${port}`) });
