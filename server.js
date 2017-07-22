b3ddcd8c3c7e06acbed172c5731f53378af61d69
Tyler Haigh <tylerhaigh1994@me.com>
2017-07-22 10:01:51 +1000

    Fiddle with package.json to launch node from heroku

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000; // Heroku port override
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.use( (req, res, next) => {
+    
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + "\n", (err) => {
+        if (err)
+            console.error('Cannot append to server.log');
+    });
+    
+    // MUST CALL NEXT or it will not return result
+    next();
+} )
+
+// app.use( (req, res, next) => {
+//     res.render('maintenance.hbs');
+// } )
+
+// middleware is executed in the order it is registered
+app.use(express.static(__dirname + "/public")); // Takes absolute path to folder
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my humble shop'
+    })
+})
+
+app.get('/about', (req, res) => {
+    // res.send('<h1>About Page</h1>')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to fulfill request'
+    })
+})
+
+app.listen(port, () => { console.log(`Server is running on port ${port}`) });
--- tssrc/server.ts
+++ tssrc/server.ts
@@ -1,65 +1,65 @@
-const express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
+// const express = require('express');
+// const hbs = require('hbs');
+// const fs = require('fs');
 
-const port = process.env.PORT || 3000; // Heroku port override
+// const port = process.env.PORT || 3000; // Heroku port override
 
-var app = express()
+// var app = express()
 
-hbs.registerPartials(__dirname + '/../views/partials'); // need to use /../ to go up one folder
+// hbs.registerPartials(__dirname + '/../views/partials'); // need to use /../ to go up one folder
 
-app.set('view engine', 'hbs');
+// app.set('view engine', 'hbs');
 
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
-});
+// hbs.registerHelper('getCurrentYear', () => {
+//     return new Date().getFullYear();
+// });
 
-hbs.registerHelper('screamIt', (text: string) => {
-    return text.toUpperCase();
-})
+// hbs.registerHelper('screamIt', (text: string) => {
+//     return text.toUpperCase();
+// })
 
-
-app.use( (req, res, next) => {
-    
-    var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`;
-
-    console.log(log);
-    fs.appendFile('server.log', log + "\n", (err) => {
-        if (err)
-            console.error('Cannot append to server.log');
-    });
-    
-    // MUST CALL NEXT or it will not return result
-    next();
-} )
 
 // app.use( (req, res, next) => {
-//     res.render('maintenance.hbs');
+    
+//     var now = new Date().toString();
+//     var log = `${now}: ${req.method} ${req.url}`;
+
+//     console.log(log);
+//     fs.appendFile('server.log', log + "\n", (err) => {
+//         if (err)
+//             console.error('Cannot append to server.log');
+//     });
+    
+//     // MUST CALL NEXT or it will not return result
+//     next();
 // } )
 
-// middleware is executed in the order it is registered
-app.use(express.static(__dirname + "/public")); // Takes absolute path to folder
+// // app.use( (req, res, next) => {
+// //     res.render('maintenance.hbs');
+// // } )
+
+// // middleware is executed in the order it is registered
+// app.use(express.static(__dirname + "/public")); // Takes absolute path to folder
 
 
-app.get('/', (req, res) => {
-    res.render('home.hbs', {
-        pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my humble shop'
-    })
-})
+// app.get('/', (req, res) => {
+//     res.render('home.hbs', {
+//         pageTitle: 'Home Page',
+//         welcomeMessage: 'Welcome to my humble shop'
+//     })
+// })
 
-app.get('/about', (req, res) => {
-    // res.send('<h1>About Page</h1>')
-    res.render('about.hbs', {
-        pageTitle: 'About Page',
-    });
-})
+// app.get('/about', (req, res) => {
+//     // res.send('<h1>About Page</h1>')
+//     res.render('about.hbs', {
+//         pageTitle: 'About Page',
+//     });
+// })
 
-app.get('/bad', (req, res) => {
-    res.send({
-        errorMessage: 'Unable to fulfill request'
-    })
-})
+// app.get('/bad', (req, res) => {
+//     res.send({
+//         errorMessage: 'Unable to fulfill request'
+//     })
+// })
 
-app.listen(port, () => { console.log(`Server is running on port ${port}`) });
+// app.listen(port, () => { console.log(`Server is running on port ${port}`) });
