e2ff933c70c1405935b6f943c3a1e8cb30caf5fb
Tyler Haigh <tylerhaigh1994@me.com>
2017-07-22 12:32:47 +1000

    Add projects page

--- server.js
+++ server.js
@@ -61,4 +61,10 @@ app.get('/bad', (req, res) => {
     })
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    })
+})
+
 app.listen(port, () => { console.log(`Server is running on port ${port}`) });
